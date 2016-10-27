7fdba52f5890af6e249323ecc92927ea82ab3f23
Kevin Wang <kevixw@gmail.com>
2016-10-26 20:06:34 -0700

    Use generated hosts file directly

--- dnsmasq/adblock-hosts-generator-bootstrap.sh
+++ dnsmasq/adblock-hosts-generator-bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# paste the following command to /jffs/script/firewall-start
+# wget --no-check-certificate -qO- 'https://raw.githubusercontent.com/kevinxw/asuswrt-merlin-bootstrap/master/dnsmasq/adblock-hosts-generator-bootstrap.sh' | sh &
+
 cd /tmp
 wget --no-check-certificate -q 'https://github.com/kevinxw/asuswrt-merlin-bootstrap/raw/master/dnsmasq/generated-files/hosts.dnsmasq.gz'
 gunzip -f ./hosts.dnsmasq.gz
